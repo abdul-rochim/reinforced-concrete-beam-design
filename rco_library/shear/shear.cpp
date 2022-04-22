e2db5138b19404cab5f7594823062ba663d4db19
Abdul Rochim <76685635+Abduls46@users.noreply.github.com>
2022-04-22 11:38:14 +0700

    update shear.cpp

--- rco_library/shear/shear.cpp
+++ rco_library/shear/shear.cpp
@@ -17,10 +17,22 @@ Shear shear_design(
 	double& s_provided,
 	const double& Pu
 	) {
-	std::cout << "\n===========================================================================\n";
-	std::cout << "starting the iteration to compute shear design of concrete beam!\n";
-	std::cout << "===========================================================================\n";
-	
+	std::ofstream out_file;
+	std::string filepath = "../input_output/output/data_output.txt";
+	out_file.open(filepath, std::ios::app);
+	if (!out_file) {
+		std::cerr << "Acces denied! file not created.\n";
+		exit(1);
+	}
+	else {
+		//std::cout << "The file to write the analysis result is available in the directory.\n";
+	}
+
+	out_file << "\n-----------------------------------------------------------------------------------\n";
+	out_file << "starting the iteration to compute shear design of concrete beam!\n";
+	out_file << "-----------------------------------------------------------------------------------\n";
+	out_file << std::fixed << std::setprecision(2);
+
 	//shear strength
 	using std::pow;
 	double Vc = 0.17 * lambda_ * pow(fc, 0.5) * b * d / 1000.; //kN
@@ -28,21 +40,21 @@ Shear shear_design(
 
 	//Check if phi_Vc >= Vu
 	if (phi_Vc < Vu) {
-		std::cout << "phi Vc : " << phi_Vc << " kN < Vu : " << Vu << " kN\n";
-		std::cout << "therefore, shear reinforcement is required!\n";
+		out_file << "phi Vc : " << phi_Vc << " kN < Vu : " << Vu << " kN\n";
+		out_file << "therefore, shear reinforcement is required!\n";
 	}
 	else {
-		std::cout << "phi Vc : " << phi_Vc << " kN > Vu : " << Vu << " kN\n";
-		std::cout << "therefore, shear reinforcement is not required!\n";
-		std::cout << "but, recommend to use the minimum shear reinforcement\n";
+		out_file << "phi Vc : " << phi_Vc << " kN > Vu : " << Vu << " kN\n";
+		out_file << "therefore, shear reinforcement is not required!\n";
+		out_file << "but, recommend to use the minimum shear reinforcement\n";
 	}
 
 	//prior to calculating shear reinforcement, check if the cross-sectional dimensions satisfy
 	double phi_Vn_maximum = phi_shear * (Vc + 0.66 * pow(fc, 0.5) * b * d / 1000.); //kN
 	try {
 		if (Vu < phi_Vn_maximum) {
-			std::cout << "phi_Vn_maximum : " << phi_Vn_maximum << " kN > Vu : " << Vu << " kN\n";
-			std::cout << "Ok therefore, cross-sectional dimensions satisfy!\n";
+			out_file << "phi_Vn_maximum : " << phi_Vn_maximum << " kN > Vu : " << Vu << " kN\n";
+			out_file << "Ok therefore, cross-sectional dimensions satisfy!\n";
 		}
 		else {
 			std::cout << "phi_Vn_maximum : " << phi_Vn_maximum << " kN < Vu : " << Vu << " kN\n";
@@ -89,12 +101,12 @@ Shear shear_design(
 		//calculate maximum allowable stirrup spacing:
 		s_req = (n_stirrup * 0.25 * M_PI * dia_stirrup * dia_stirrup) / Av_per_s_req;
 	}
-	std::cout << "initial s required(maximum allowable stirrup spacing): " << s_req << " mm\n";
+	out_file << "initial s required(maximum allowable stirrup spacing): " << s_req << " mm\n";
 
 	if (s_provided < 40. || s_provided > 600.)
 		s_provided = 600.; //assumption initial s
 
-	std::cout << "initial s provided : " << s_provided << " mm\n\n";
+	out_file << "initial s provided : " << s_provided << " mm\n\n";
 
 	//first does the beam transverse reinforcement value need to exceed
 	//the threshold value of Vs <= 0.33 * sqrt(fc') bd?
@@ -104,13 +116,13 @@ Shear shear_design(
 	//In the region where Vu <= phi_Vc /2, shear reinforcement is not required.
 	//Check is Vu <= phi_Vc /2 ?
 	if (Vu <= phi_Vc / 2.) {
-		std::cout << "Vu : " << Vu << " kN < phi_Vc/2 : " << phi_Vc / 2. << " kN\n";
-		std::cout << "therefore, shear reinforcement is not required!\n";
-		std::cout << "but, recommend to use minimum shear reinforcement\n\n";
+		out_file << "Vu : " << Vu << " kN < phi_Vc/2 : " << phi_Vc / 2. << " kN\n";
+		out_file << "therefore, shear reinforcement is not required!\n";
+		out_file << "but, recommend to use minimum shear reinforcement\n\n";
 	}
 	else {
-		std::cout << "Vu : " << Vu << " kN > phi_Vc/2 : " << phi_Vc / 2. << " kN\n";
-		std::cout << "check, is Av_provided greater than minimum shear reinforcement?\n\n";
+		out_file << "Vu : " << Vu << " kN > phi_Vc/2 : " << phi_Vc / 2. << " kN\n";
+		out_file << "check, is Av_provided greater than minimum shear reinforcement?\n\n";
 	}
 
 	//specified shear reinforcement must be at least:
@@ -135,21 +147,21 @@ Shear shear_design(
 	//iteration for computing s provided dan check to the minimum shear bars
 	unsigned int j = 1;
 	do {
-		std::cout << "iteration_" << j << '\n';
+		out_file << "iteration_" << j << '\n';
 
 		Av_per_s_provided = Av_prov / s_provided;
-		std::cout << "Av_per_s_provided : " << Av_per_s_provided << " mm2/m\n";
+		out_file << "Av_per_s_provided : " << Av_per_s_provided << " mm2/m\n";
 
 		//Vs = Av * fyt * d/s
 		Vs_provided = Av_prov * fyv * d / s_provided /1000.; //kN
-		std::cout << "Vs_provided : " << Vs_provided << " kN\n";
+		out_file << "Vs_provided : " << Vs_provided << " kN\n";
 
 		//first does the beam transverse reinforcement value need to exceed
 		//the threshold value of Vs <= 0.33 * sqrt(fc') bd?
 		//Vs provided should be lesser than Vs maximum
 		try {
 			if (Vs_provided <= Vs_maximum)
-				std::cout << "Ok, Vs_provided : " << Vs_provided << " kN < Vs maximum : " << Vs_maximum << " kN\n";
+				out_file << "Ok, Vs_provided : " << Vs_provided << " kN < Vs maximum : " << Vs_maximum << " kN\n";
 			else {
 				std::cout << "Vs_provided : " << Vs_provided << " kN > Vs maximum : " << Vs_maximum << " kN\n";
 				std::cout << "enlarge cross-sectional dimensions\n";
@@ -164,52 +176,53 @@ Shear shear_design(
 
 		//check Av per s minimum
 		if (Av_per_s_provided >= Av_per_s_minimum) {
-			std::cout << "Av_per_s prov : " << Av_per_s_provided << " mm2/mm > Av_per_s_minimum: " << Av_per_s_minimum << " mm2/mm\n";
-			std::cout << "Ok, stirrup spacing satisfies the minimum shear reinforcement\n";
+			out_file << "Av_per_s prov : " << Av_per_s_provided << " mm2/mm > Av_per_s_minimum: " << Av_per_s_minimum << " mm2/mm\n";
+			out_file << "Ok, stirrup spacing satisfies the minimum shear reinforcement\n";
 		}
 		else {
-			std::cout << "Av_per_s prov : " << Av_per_s_provided << " mm2/mm < Av_per_s_minimum: " << Av_per_s_minimum << " mm2/mm\n";
-			std::cout << "stirrup spacing is lesser than the minimum shear reinforcement\n";
+			out_file << "Av_per_s prov : " << Av_per_s_provided << " mm2/mm < Av_per_s_minimum: " << Av_per_s_minimum << " mm2/mm\n";
+			out_file << "stirrup spacing is lesser than the minimum shear reinforcement\n";
 		}
-		std::cout << "s required : " << s_req << " mm\n";
+		out_file << "s required : " << s_req << " mm\n";
 		if (s_provided > s_req)
-			std::cout << "s provided iteration : " << s_provided << " mm > s required\n";
+			out_file << "s provided iteration : " << s_provided << " mm > s required\n";
 		else
-			std::cout << "s provided iteration : " << s_provided << " mm < s required\n";
+			out_file << "s provided iteration : " << s_provided << " mm < s required\n";
 		
 		//because the required shear strength is below the threshold value,
 		//the maximum stirrup spacing is the lesser of d/2 and 600 mm, and Av_prov/(Av/s minimum)
-		std::cout << "\ns maximum requirement :\n";
-		std::cout << "s provided must be lesser than d/2 : " << d / 2. << " mm and " << 600.0 << " mm\n";
-		std::cout << "s provided must be lesser than Av prov/ (Av/s min) : " << Av_prov / Av_per_s_minimum << " mm\n";
+		out_file << "\ns maximum requirement :\n";
+		out_file << "s provided must be lesser than d/2 : " << d / 2. << " mm and " << 600.0 << " mm\n";
+		out_file << "s provided must be lesser than Av prov/ (Av/s min) : " << Av_prov / Av_per_s_minimum << " mm\n";
 		//display s maximum required
-		std::cout << "s maximum required : " << s_max << " mm\n";
+		out_file << "s maximum required : " << s_max << " mm\n";
 
 		if(s_provided > s_max )
-			std::cout << "s provided iteration : " << s_provided << " mm > s maximum\n";
+			out_file << "s provided iteration : " << s_provided << " mm > s maximum\n";
 		else
-			std::cout << "s provided iteration : " << s_provided << " mm < s maximum\n";
+			out_file << "s provided iteration : " << s_provided << " mm < s maximum\n";
 
 		//compute nominal shear capacity
 		phi_Vn_provided = phi_shear * (Vc + Vs_provided);
 		//check is Vu < phi Vn ?
 		if (phi_Vn_provided >= Vu) {
-			std::cout << "\nphi Vn provided iteration : " << phi_Vn_provided << " kN > Vu : " << Vu << " kN\n";
+			out_file << "\nphi Vn provided iteration : " << phi_Vn_provided << " kN > Vu : " << Vu << " kN\n";
 			if (s_provided > s_max)
-				std::cout << "however s provided > s max; therefore re-calculate again!\n";
+				out_file << "however s provided > s max; therefore re-calculate again!\n";
 		}
 		else {
-			std::cout << "\ns provided > s required\n";
-			std::cout << "phi Vn provided iteration : " << phi_Vn_provided << " kN < Vu : " << Vu << " kN, therefore re-calculate again!\n";
+			out_file << "\ns provided > s required\n";
+			out_file << "phi Vn provided iteration : " << phi_Vn_provided << " kN < Vu : " << Vu << " kN, therefore re-calculate again!\n";
 		}
-		std::cout << '\n';
+		out_file << '\n';
 
 		s_provided = s_provided - 10.;
 		++j;
 	} while (s_provided + 10. > s_max || phi_Vn_provided < Vu);	//s should be lesser than s_max, and phi Vn provided should be greater than Vu.
 
-	std::cout << "calculation of the nominal shear capacity is complete!\n";
-	std::cout << "===========================================================================\n";
+	out_file << "calculation of the nominal shear capacity complete!\n";
+	out_file << "-----------------------------------------------------------------------------------\n";
 	s_provided = s_provided + 10.;
+	//out_file.close();
 	return { Vc, Vs_provided, Av_per_s_req, Av_per_s_provided };	//return rvalue (using constructor) //return Shear{ Vc, Vs_provided, Av_per_s_req, Av_per_s_provided };
 }
