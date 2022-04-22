c6818cac261b25c0c66661a8d6d121a06955a247
Abdul Rochim <76685635+Abduls46@users.noreply.github.com>
2022-04-22 11:23:15 +0700

    update moment.cpp

--- rco_library/moment/moment.cpp
+++ rco_library/moment/moment.cpp
@@ -37,7 +37,7 @@ void Moment::swap(Moment& other) noexcept {
 	swap(this->Ec, other.Ec);
 }
 
-inline double phi_moment(Moment m, const double& As_, double& eps_t_) {
+inline double phi_moment(Moment& m, const double& As_, double& eps_t_) {
 	double eps_ty{};
 	if (m.fy == 420)
 		eps_ty = double(0.002);
@@ -62,37 +62,50 @@ inline double phi_moment(Moment m, const double& As_, double& eps_t_) {
 }
 
 double phiMn(
-	Moment m, 
+	Moment& m, 
 	const double& dia_long, 
 	const double& d_hoop, 
 	const double& cover, 
 	double* As_required, 
 	double* nb_bar
 	) {
-	std::cout << "===========================================================================\n";
-	std::cout << "Starting the iteration to compute nominal moment capacity of concrete beam!\n";
-	std::cout << "===========================================================================\n";
-	std::cout << "initial As longitudinal required : " << m.initial_As_req() << " mm2\n";
+	std::ofstream out_file;
+	std::string filepath = "../input_output/output/data_output.txt";
+	out_file.open(filepath, std::ios::app);
+	if (!out_file) {
+		std::cerr << "Acces denied! file not created.\n";
+		exit(1);
+	}
+	else {
+		//std::cout << "The file to write the analysis result is available in the directory.\n";
+	}
+
+	out_file << "-----------------------------------------------------------------------------------\n";
+	out_file << "Starting the iteration to compute nominal moment capacity of concrete beam!\n";
+	out_file << "-----------------------------------------------------------------------------------\n";
+	out_file << std::fixed << std::setprecision(2);
+
+	out_file << "initial As longitudinal required : " << m.initial_As_req() << " mm2\n";
 
 	//compute number of bar reinforcement required
 	double n_ = m.initial_As_req() / (double(0.25) * M_PI * dia_long * dia_long);
-	std::cout << "initial number of longitudinal bars required : " << n_ << " bars\n";
+	out_file << "initial number of longitudinal bars required : " << n_ << " bars\n";
 	if(n_ > 0.)
-		std::cout << "initial number of longitudinal bars required(roundup) : " << std::ceil(n_) << " bars\n";
+		out_file << "initial number of longitudinal bars required(roundup) : " << std::ceil(n_) << " bars\n";
 	
-	std::cout << "initial number of bars provided : " << *nb_bar << " bars\n";
+	out_file << "initial number of bars provided : " << *nb_bar << " bars\n";
 
 	if (*nb_bar >= n_) {
-		std::cout << "if initial number of bars provided : " << *nb_bar << " bars > initial number of bars required : " << n_ << " bars\n";
-		std::cout << "assumption, initial number of bars required is using number of bars provided : " << *nb_bar << " bars\n";
+		out_file << "if initial number of bars provided : " << *nb_bar << " bars > initial number of bars required : " << n_ << " bars\n";
+		out_file << "assumption, initial number of bars required is using number of bars provided : " << *nb_bar << " bars\n";
 		n_ = *nb_bar;
 	}
 	else if (n_ <= 1) {
-		std::cout << "if initial number of bars required : " << n_ << " bars <= 1\n";
-		std::cout << "initial number of bars required used : " << 2.0 << " bars\n";
+		out_file << "if initial number of bars required : " << n_ << " bars <= 1\n";
+		out_file << "initial number of bars required used : " << 2.0 << " bars\n";
 		n_ = 2.0; //minimum number of bar reinforcement placed at both corner
 	}
-	std::cout << '\n';
+	out_file << '\n';
 
 	//declare variables of As_req_iteration, As_prov, phiMn
 	double As_req_iteration;
@@ -151,25 +164,25 @@ double phiMn(
 	//iteration for computing phi Mn dan check to the minimum longitudinal bars
 	unsigned int i = 1;
 	do {
-		std::cout << "iteration_" << i << '\n';
-		std::cout << "n longitudinal required from the iteration : " << n_ << " bars\n";
-		std::cout << "n longitudinal provided from the iteration : " << std::ceil(n_) << " bars\n";
+		out_file << "iteration_" << i << '\n';
+		out_file << "n longitudinal required from the iteration : " << n_ << " bars\n";
+		out_file << "n longitudinal provided from the iteration : " << std::ceil(n_) << " bars\n";
 		As_req_iteration = n_ * double(0.25) * M_PI * dia_long * dia_long;
 
 		//assigment of bars area requirement
 		*As_required = As_req_iteration;
 
 		As_prov = std::ceil(n_) * double(0.25) * M_PI * dia_long * dia_long; //round up from As required or As required iteration
-		std::cout << "As longitudinal required from the iteration : " << As_req_iteration << " mm2\n";
-		std::cout << "As longitudinal provided from the iteration : " << As_prov << " mm2\n";
+		out_file << "As longitudinal required from the iteration : " << As_req_iteration << " mm2\n";
+		out_file << "As longitudinal provided from the iteration : " << As_prov << " mm2\n";
 
 		//beam's web required, bw_req = 2 (cover + d_stirrup + db) + (n-1) * db + (n-1) * clear spacing
 		//assume n_dummy = n - 1
 		n_dummy = (m.b - double(2) * (cover + d_hoop + dia_long)) / (dia_long + clear_spacing_long);
 		//re-calculate n_dummy;
 		n_dummy = n_dummy + 1.0;
-		std::cout << "maximum number of bars per layer from the calculation : " << n_dummy << " bars per layer\n";
-		std::cout << "maximum number of bars per layer recommended : " << std::trunc(n_dummy) << " bars per layer\n";
+		out_file << "maximum number of bars per layer from the calculation : " << n_dummy << " bars per layer\n";
+		out_file << "maximum number of bars per layer recommended : " << std::trunc(n_dummy) << " bars per layer\n";
 
 		//compute the distribution of bar layer
 		if (std::ceil(n_) <= std::trunc(n_dummy)) {
@@ -220,25 +233,25 @@ double phiMn(
 			n1 = std::trunc(n_dummy);
 			n2 = std::ceil(n_) - n1;
 		}
-		std::cout << "number of bars, layer-1 : " << n1 << " bars" <<
+		out_file << "number of bars, layer-1 : " << n1 << " bars" <<
 			"\nnumber of bars, layer-2 : " << n2 << " bars" <<
 			"\nnumber of bars, layer-3 : " << n3 << " bars\n";// <<
 //			"\nnumber of bars, layer-4 : " << n4 << " bars\n";
 		
 		//display minimum and maximum clear spacing bar 
-		std::cout << "minimum clear spacing bar : " << clear_spacing_long << " mm\n";
-		std::cout << "maximum bar spacing : " << max_bar_spacing << " mm\n";
+		out_file << "minimum clear spacing bar : " << clear_spacing_long << " mm\n";
+		out_file << "maximum bar spacing : " << max_bar_spacing << " mm\n";
 
 		//compute clear spacing bar provided
 		//clear_spacing_provided = (m.b - double(2) * (cover + d_hoop + dia_long) - (std::max(n1, std::max(n2, std::max(n3, n4))) - 1.0) * dia_long) / (std::max(n1, std::max(n2, std::max(n3, n4))) - 1.0);
 		clear_spacing_provided = (m.b - double(2) * (cover + d_hoop + dia_long) - (std::max(n1, std::max(n2, n3)) - 1.0) * dia_long) / (std::max(n1, std::max(n2, n3)) - 1.0);
-		std::cout << "clear spacing bar provided : " << clear_spacing_provided << " mm\n";
+		out_file << "clear spacing bar provided : " << clear_spacing_provided << " mm\n";
 		
 		//check maximum bar spacing!
 		//this limit is intended to control flexural cracking width.
 		try {
 			if (clear_spacing_provided < max_bar_spacing)
-				std::cout << "clear spacing bar provided : " << clear_spacing_provided << " mm < max bar spacing : " << max_bar_spacing << " mm\n";
+				out_file << "clear spacing bar provided : " << clear_spacing_provided << " mm < max bar spacing : " << max_bar_spacing << " mm\n";
 			else {
 				std::cout << "clear spacing bar provided : " << clear_spacing_provided << " mm > max bar spacing : " << max_bar_spacing << " mm\n";
 				std::cout << "need to add longitudinal bar(s)\n";
@@ -253,7 +266,7 @@ double phiMn(
 		//check minimum bar spacing!
 		try {
 			if (clear_spacing_provided > clear_spacing_long)
-				std::cout << "clear spacing bar provided : " << clear_spacing_provided << " mm > min bar spacing : " << clear_spacing_long << " mm\n";
+				out_file << "clear spacing bar provided : " << clear_spacing_provided << " mm > min bar spacing : " << clear_spacing_long << " mm\n";
 			else {
 				std::cout << "clear spacing bar provided : " << clear_spacing_provided << " mm < min bar spacing : " << clear_spacing_long << " mm\n";
 				std::cout << "enlarge the cross-sectional dimensions\n";
@@ -302,9 +315,9 @@ double phiMn(
 		//calculation or assignment of epsilon t, a, phi
 		a_ = m.a(As_prov);
 		phi_ = phi_moment(m, As_prov, eps_t);
-		std::cout << std::fixed << std::setprecision(4);
-		std::cout << "\nepsilon t calc' : " << eps_t << '\n'; //epsilon t for computing rho maximum per rho balance
-		std::cout << std::fixed << std::setprecision(2);
+		out_file << std::fixed << std::setprecision(4);
+		out_file << "\nepsilon t calc' : " << eps_t << '\n'; //epsilon t for computing rho maximum per rho balance
+		out_file << std::fixed << std::setprecision(2);
 
 		//compute nominal moment capacity
 //		phiMn = phi_ * (As1 * m.fy * d1 + As2 * m.fy * d2 + As3 * m.fy * d3 + As4 * m.fy * d4 - (As1 + As2 + As3 + As4) * m.fy * a_ / 2.0) / 1000000.0; //kN.m
@@ -312,13 +325,13 @@ double phiMn(
 
 		//Checking the reinforcement ratio
 		//minimum reinforcement ratio
-		std::cout << "minimum and maximum reinforcement ratio:\n";
+		out_file << "minimum and maximum reinforcement ratio:\n";
 		As_min1 = 0.25 * std::pow(m.fc, 0.5) / m.fy * m.b * m.d;
 		As_min2 = 1.4 / m.fy * m.b * m.d;
 		As_minimum = std::max(As_min1, As_min2);
-		std::cout << "As minimum requirement 1 [0.25 sqrt(fc')/fy *bd] : " << As_min1 << " mm2\n";
-		std::cout << "As minimum requirement 2 [1.4/fy *bd] : " << As_min2 << " mm2\n";
-		std::cout << "As minimum requirement [used] : " << As_minimum << " mm2\n";
+		out_file << "As minimum requirement 1 [0.25 sqrt(fc')/fy *bd] : " << As_min1 << " mm2\n";
+		out_file << "As minimum requirement 2 [1.4/fy *bd] : " << As_min2 << " mm2\n";
+		out_file << "As minimum requirement [used] : " << As_minimum << " mm2\n";
 
 		//maximum reinforcement ratio
 		//calculate epsilon t for defining rho max per rho balance
@@ -335,27 +348,27 @@ double phiMn(
 		rho_balance = 0.85 * m.betha1 * m.fc / m.fy * (600 / (600 + m.fy));	//rho balance
 		rho_max_per_rho_balance = (0.003 + m.fy / m.Es) / (0.003 + eps_t);	//rho maximum per rho balance
 		rho_maximum = rho_max_per_rho_balance * rho_balance;	//rho maximum
-		std::cout << std::fixed << std::setprecision(4);
-		std::cout << "epsilon t for computing rho maximum: " << eps_t << '\n';
-		std::cout << "rho maximum per rho balance : " << rho_max_per_rho_balance << '\n';
-		std::cout << "rho maximum : " << rho_maximum << '\n';
+		out_file << std::fixed << std::setprecision(4);
+		out_file << "epsilon t for computing rho maximum: " << eps_t << '\n';
+		out_file << "rho maximum per rho balance : " << rho_max_per_rho_balance << '\n';
+		out_file << "rho maximum : " << rho_maximum << '\n';
 		As_maximum = rho_maximum * m.b * m.d;
-		std::cout << std::fixed << std::setprecision(2);
-		std::cout << "As maximum requirement : " << As_maximum << " mm2\n";
+		out_file << std::fixed << std::setprecision(2);
+		out_file << "As maximum requirement : " << As_maximum << " mm2\n";
 
 		//error handling
 		try {
 			if (As_prov >= As_minimum && As_prov <= As_maximum) {
-				std::cout << "Ok, As minimum : " << As_minimum << " mm2 < " << "As longitudinal reinforcement provided : " << As_prov << " mm2 < " << "As maximum : " << As_maximum << " mm2\n";
+				out_file << "Ok, As minimum : " << As_minimum << " mm2 < " << "As longitudinal reinforcement provided : " << As_prov << " mm2 < " << "As maximum : " << As_maximum << " mm2\n";
 			}
 			else if (As_prov < As_minimum && phiMn >= 4.0 / 3.0 * m.Mu) {
-				std::cout << "As longitudinal reinforcement provided : " << As_prov << " < As minimum : " << As_minimum << " and phiMn : " << phiMn << " kN.m > (4/3) Mu : " << 4. / 3 * m.Mu << " kN.m\n";
-				std::cout << "Ok, the element structure is big and massive\n";
+				out_file << "As longitudinal reinforcement provided : " << As_prov << " < As minimum : " << As_minimum << " and phiMn : " << phiMn << " kN.m > (4/3) Mu : " << 4. / 3 * m.Mu << " kN.m\n";
+				out_file << "Ok, the element structure is big and massive\n";
 			}
 			else if (As_prov < As_minimum)
 			{
-				std::cout << "As_provided : " << As_prov << " mm2 < As_minimum : " << As_minimum << " mm2\n";
-				std::cout << "reinforcement ratio provided is lesser then minimum reinforcement ratio\n";
+				out_file << "As_provided : " << As_prov << " mm2 < As_minimum : " << As_minimum << " mm2\n";
+				out_file << "reinforcement ratio provided is lesser then minimum reinforcement ratio\n";
 			}
 			else //if(As_prov > As_maximum)
 			{
@@ -372,38 +385,39 @@ double phiMn(
 		}
 
 		//check assumption of phi_moment
-		std::cout << "\ncheck the assumption of phi!\n";
+		out_file << "\ncheck the assumption of phi!\n";
 		if (phi_ == 0.9)
-			std::cout << "the design assumption of using phi is 0.9 in tension-controlled is verified, is phiMn > Mu ?\n";
+			out_file << "the design assumption of using phi is 0.9 in tension-controlled is verified, is phiMn > Mu ?\n";
 		else {
-			std::cout << "the phi calculation is " << phi_ << " , is phiMn > Mu ?\n";
+			out_file << "the phi calculation is " << phi_ << " , is phiMn > Mu ?\n";
 		}
 		
 		//check is phi Mn > Mu and As provided?
 		if (phiMn >= 4./3 * m.Mu && As_prov < As_minimum) {
-			std::cout << "whereas As_provided: " << As_prov << " mm2 << As_minimum: " << As_minimum << " mm2\n";
-			std::cout << "but it's Ok, since phiMn: " << phiMn << " kN.m > 4/3 Mu: " << 4./3 * m.Mu << " kN.m ";
-			std::cout << "[massive structure]\n";
+			out_file << "whereas As_provided: " << As_prov << " mm2 << As_minimum: " << As_minimum << " mm2\n";
+			out_file << "but it's Ok, since phiMn: " << phiMn << " kN.m > 4/3 Mu: " << 4./3 * m.Mu << " kN.m ";
+			out_file << "[massive structure]\n";
 		}
 		else if (phiMn >= m.Mu && As_prov >= As_minimum) {
-			std::cout << "Ok, phiMn: " << phiMn << " kN.m > Mu: " << m.Mu << " kN.m ";
-			std::cout << "and As_provided: " << As_prov << " mm2 > As_minimum: " << As_minimum << " mm2\n";
+			out_file << "Ok, phiMn: " << phiMn << " kN.m > Mu: " << m.Mu << " kN.m ";
+			out_file << "and As_provided: " << As_prov << " mm2 > As_minimum: " << As_minimum << " mm2\n";
 		}
 		else if (phiMn < m.Mu && As_prov > As_minimum) {
-			std::cout << "As_provided: " << As_prov << " mm2 > As_minimum: " << As_minimum << " mm2\n";
-			std::cout << "but phiMn: " << phiMn << " kN.m < Mu: " << m.Mu << " kN.m, re-calculate again!\n";
+			out_file << "As_provided: " << As_prov << " mm2 > As_minimum: " << As_minimum << " mm2\n";
+			out_file << "but phiMn: " << phiMn << " kN.m < Mu: " << m.Mu << " kN.m, re-calculate again!\n";
 		}
 		else {
-			std::cout << "phiMn: " << phiMn << " kN.m > Mu: " << m.Mu << " kN.m\n";
-			std::cout << "but As_provided: " << As_prov << " mm2 < As_minimum: " << As_minimum << " mm2, re-calculate again!\n";
+			out_file << "phiMn: " << phiMn << " kN.m > Mu: " << m.Mu << " kN.m\n";
+			out_file << "but As_provided: " << As_prov << " mm2 < As_minimum: " << As_minimum << " mm2, re-calculate again!\n";
 		}
 
-		std::cout << '\n';
+		out_file << '\n';
 		++n_;
 		++i;
 	} while ((phiMn < m.Mu && As_prov < As_minimum) || phiMn < m.Mu || (phiMn < 4./3. * m.Mu && As_prov < As_minimum));
 	
-	std::cout << "calculation of the nominal moment capacity is complete!\n";
-	std::cout << "===========================================================================\n";
+	out_file << "calculation of the nominal moment capacity complete!\n";
+	out_file << "-----------------------------------------------------------------------------------\n";
+	//out_file.close();
 	return phiMn;
 }
